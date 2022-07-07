217630e2e445edb786813a6416693d722ed38c9e
Erik <erikbarros1402@gmail.com>
2022-07-07 17:00:26 -0300

    novo feedback

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,13 +1,16 @@
 import { ChatTeardropDots } from 'phosphor-react'
 
 import { Popover } from '@headlessui/react'
+import { Widgetform } from './WidgetForm'
 
 export function Widget() {
  
 
     return (
         <Popover className='absolute bottom-4 right-4 md:bottom-11 md:right-11 first-letter:flex flex-col items-end' >
-           <Popover.Panel>yaeeee</Popover.Panel>
+           <Popover.Panel>
+            <Widgetform/>
+           </Popover.Panel>
         <Popover.Button  className='bg-brand-500 rounded-full  px-3 h-12 text-white flex items-center  group'>
             <ChatTeardropDots  size={50} className='w-6 h-6' />
             <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
